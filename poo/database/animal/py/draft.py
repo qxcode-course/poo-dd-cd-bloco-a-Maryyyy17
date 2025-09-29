9f58db5e2e0442590112ca63f6e2423f5082a1cb
Maryyyy17 <liberatomari35@gmail.com>
2025-09-29 19:11:36 +0000

    alteraçoes toalha e animal

---
+++ poo/database/animal/py/draft.py
@@ -0,0 +1,9 @@
+claass Animal :
+def__init__(self,species: str, sound: str):
+    self.species : str = species
+    self.sound : str = sound
+    self.age : int = 0
+
+
+def__str__(self) :
+    return f"[self]"
--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,17 +1,44 @@
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color: str = color
+        self.size : str = size
+        self.wetness : int = 0
 
+    def dry(self,amount:int) -> None:
+        self.wetness += amount
+        if self.wetness >= self.getMaxWetness():
+            print("toalha enxarcada")
+            self.wetness = self.getMaxWetness()
 
-class Toalha:
-    def __init__(self, color:str, size:str): #construtor
-        self.color = "red" #atributos
-        self.size = "p"
-        self.wetness = 0
+    def getMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return  20
+        if self.size == "G":
+            return  30
+        return 0
 
-    def __str__(self):
-        return f"color:{self.color}, tam:{self.size}, hum{self.wetness}"
+    def __str__(self) -> str:
+        return f"Cor:{self.color}, Tam:{self.size}, Umidade:{self.wetness}"
 
-towel = Toalha("green", "G") #objetos
-towel2 = Toalha("purple", "M")
-towel.color = "white"
-print(towel.color)
-print(towel.size)
-print(towel.wetness)
+def main():
+    toalha = Towel("", "")
+    while True:
+        line: str = input()
+        args: list[str] = line.split(" ")
+        if args[0] == "end" :
+            break
+        elif args [0] == "new":
+            color = args[1]
+            size = args[2]
+            toalha = Towel(color, size)
+        elif args[0] == "show":
+            print(toalha)
+        elif args[0] == "dry":
+            amount: int = int(args[1])
+            toalha.dry(amount)
+        else:
+            print("fail: código invalido")
+
+main()
